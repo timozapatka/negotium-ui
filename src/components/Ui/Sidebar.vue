b14eeb232b957269de4693862363a9223baf32f1
Frank Köppert <info@kdc-lab.de>
2021-03-07 11:51:50 +0100

    KDCLNUI-7 add theme component to Sidebar

--- src/components/Ui/Sidebar.vue
+++ src/components/Ui/Sidebar.vue
@@ -6,6 +6,7 @@
         <q-item-section>
           <q-expansion-item icon="settings" :label="$t('uiSettings')">
             <language />
+            <theme />
           </q-expansion-item>
         </q-item-section>
       </q-item>
@@ -17,12 +18,14 @@
 <script>
 import { defineComponent } from 'vue';
 import Language from './Uitlities/Language.vue';
+import Theme from './Uitlities/Theme.vue';
 
 export default defineComponent({
   name: 'Sidebar',
 
   components: {
     Language,
+    Theme,
   },
 
   setup() {
---
+++ src/components/Ui/Uitlities/Theme.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-item class="q-ma-sm q-pa-sm" clickable>
+    <q-item-section avatar>
+      <q-avatar icon="invert_colors" />
+    </q-item-section>
+    <q-item-section>
+      <q-toggle v-model="dark" @click="toggle()" />
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { useQuasar } from 'quasar';
+
+export default defineComponent({
+  name: 'Theme',
+  setup() {
+    const $q = useQuasar();
+    const dark = $q.dark.isActive;
+
+    function toggle() {
+      $q.dark.toggle();
+      dark = $q.dark.isActive;
+    }
+
+    return {
+      dark,
+      toggle,
+    };
+  },
+});
+</script>
